c67d27793d4abbaf9c069e001898643c1c99aa4e
Tom Butterwith <thomas.butterwith@skyscanner.net>
2021-12-23 14:38:15 +0000

    migrates the whole site to Gatsby

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # butterwith.space
 
 Repository containing the code for https://butterwith.space
+
+## Running
+
+This blog is developed using (Gatsby)[https://www.gatsbyjs.com] and uses the (LekoArts/gatsby-starter-minimal-blog)[https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-minimal-blog] theme.
+
+### Commands
+
+```sh
+$ npm run develop # Run the project locally and view a live preview at http://localhost:8000
+
+$ npm run build # Build the project ready for deployment
+```
---
+++ gatsby-config.js
@@ -0,0 +1,128 @@
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Tom Butterwith`,
+    siteTitleAlt: `Tom Butterwith - butterwith.space`,
+    siteHeadline: `Tom Butterwith - butterwith.space`,
+    siteUrl: `https://butterwith.space`,
+    siteDescription: `Articles and projects from Tom Butterwith`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: ``,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.gstatic.com`],
+        interval: 300,
+        timeout: 30000,
+        // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
+        // See: https://github.com/LekoArts/gatsby-themes/tree/main/examples/minimal-blog#changing-your-fonts
+        web: [
+          {
+            name: `IBM Plex Sans`,
+            file: `https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tom Butterwith - butterwith.space`,
+        short_name: `butterwith.space`,
+        description: `The personal website for Tom Butterwith`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allPost } }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                }
+              }),
+            query: `
+              {
+                allPost(sort: { fields: date, order: DESC }) {
+                  nodes {
+                    title
+                    date(formatString: "MMMM D, YYYY")
+                    excerpt
+                    slug
+                  }
+                }
+              }
+            `,
+            output: `rss.xml`,
+            title: `Tom Butterwith - butterwith.space`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
