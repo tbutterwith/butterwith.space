2770089530b489065565df84c13a5ae97aaf03a8
Tom Butterwith <thomas.butterwith@skyscanner.net>
2021-12-23 19:54:40 +0000

    downloads the current day's word

--- netlify/functions/word_wheel.get.js
+++ netlify/functions/word_wheel.get.js
@@ -1,9 +1,30 @@
-exports.handler = async function(event, context) {
-  const data = {word: "UNRUFFLED"};
-  
-  return {
-		statusCode:200,
-		body:JSON.stringify(data),
-    headers: {'Content-type': 'application/JSON'}
-	};
+import * as AWS from 'aws-sdk';
+import { format } from 'date-fns';
+
+exports.handler = function(event, context, callback) {
+  AWS.config.update({
+    region: "us-east-1",
+    accessKeyId: process.env.WW_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.WW_AWS_SECRET_ACCESS_KEY,
+  });
+
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  const date = format(new Date()  , 'yyyy-MM-dd');
+
+  docClient.get({TableName: 'word_wheel', Key:{id: date}}, (err, data) =>{
+    if (err) {
+      console.error(err);
+      callback(null, {
+        statusCode: 500
+      });
+    }
+
+    const { Item } = data;
+    
+    callback(null, {
+      statusCode: 200,
+      body:JSON.stringify(Item),
+      headers: {'Content-type': 'application/JSON'}
+    });
+  });
 }
--- scripts/populate_word_wheel_db.js
+++ scripts/populate_word_wheel_db.js
@@ -1,9 +1,27 @@
 var AWS = require("aws-sdk");
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-  }
+AWS.config.update({
+  region: "us-east-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: 'word_wheel',
+  Item: {
+    id: '2021-12-23',
+    word: 'UNRUFFLED',
+  },
+}
+
+// docClient.put(params, (err, data) => {
+//   console.log('err', err);
+//   console.log('data', data);
+// })
+
+docClient.get({TableName: 'word_wheel', Key:{id: '2021-12-23'}}, (err, data) =>{
+  console.log('err', err);
+  console.log('data', data);
+})
