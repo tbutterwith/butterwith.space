cd0915de8d01ea4f479bba7c87fd56faf7dbf9fb
Tom Butterwith <tbutterwith@gmail.com>
2023-06-01 14:28:52 +0100

    adds favicons

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,23 @@ export default function AppOverride({ Component, pageProps }) {
     <>
       <Head>
         <title>Tom Butterwith - butterwith.space</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/icons/favicon-16x16.png"
+        />
       </Head>
       <div className="px-10 py-10">
         <HeaderLinks />
