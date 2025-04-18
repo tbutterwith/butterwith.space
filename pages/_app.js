1e88f0aca3de0d305da6c4e5070cbde4b49529fd
Tom Butterwith <tbutterwith@gmail.com>
2025-04-18 10:29:32 +0100

    adds Posthog metrics and logging

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,34 @@ import Head from "next/head";
 import HeaderLinks from "../components/header-links";
 import "../styles/globals.css";
 
+import { useEffect } from "react";
+import { Router } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
 export default function AppOverride({ Component, pageProps }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+      person_profiles: "always",
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+
+    const handleRouteChange = () => posthog?.capture("$pageview");
+
+    Router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <Head>
         <title>Tom Butterwith - butterwith.space</title>
         <link
@@ -29,6 +54,6 @@ export default function AppOverride({ Component, pageProps }) {
         <HeaderLinks />
         <Component {...pageProps} />
       </div>
-    </>
+    </PostHogProvider>
   );
 }
